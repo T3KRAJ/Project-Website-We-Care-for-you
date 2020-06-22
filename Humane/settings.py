0569556367ec3dc71f26e5bedfc7dacacc19237f
Tekraj Joshi <tekrajjoshi07@gmail.com>
2020-06-22 12:15:37 +0530

    Debug mode changed

--- Humane/settings.py
+++ Humane/settings.py
@@ -8,9 +8,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nj#=#)w($nn1kiw+4g@^lq_)6(ed6)q_mg1midi8ff7gw=-h!u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['humane.pythonanywhere.com']
 
 
 # Application definition
